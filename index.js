f7c18b1290759818e6234f9e5fefe4d630371a1b
Connor Burns <cpburns17@gmail.com>
2023-10-14 10:59:26 -0400

    complete lab

---
+++ index.js
@@ -0,0 +1,8 @@
+
+
+  function introductionWithLanguage(name, language = "JavaScript") {
+    console.log(`"Hi, my name is ${name} and I am learning to program in ${language}."`)
+  }
+
+  introductionWithLanguage("Connor");
+  
